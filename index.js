49d35e97f4bb23e9803032aece9232415650e027
Oskar Risberg <oskar.risberg@stendahls.se>
2022-08-17 08:06:11 +0200

    switch secrets provider

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const { App } = require('@slack/bolt');
 const Pusher = require('pusher');
+const dockerSecret = require('@stendahls/docker-secrets-or-dotenv');
 
 const emoji = require('./modules/emoji');
 
 const app = new App({
-    signingSecret: process.env.SLACK_SIGNING_SECRET,
-    token: process.env.SLACK_BOT_TOKEN,
+    signingSecret: dockerSecret('SLACK_SIGNING_SECRET'),
+    token: dockerSecret('SLACK_BOT_TOKEN'),
 });
 
 const pusher = new Pusher({
     appId: puser.env.PUSHER_APP_ID,
     key: '518ab0476ccf565431b1',
-    secret: process.env.PUSHER_SECRET,
+    secret: dockerSecret('PUSHER_SECRET'),
     cluster: 'mt1',
     // useTLS: USE_TLS, // optional, defaults to false
     // encryptionMasterKeyBase64: ENCRYPTION_MASTER_KEY, // a base64 string which encodes 32 bytes, used to derive the per-channel encryption keys (see below!)
